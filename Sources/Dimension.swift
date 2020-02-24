32775c84e10488fdb97428cb747a3b41518fdfed
Wellington Marthas <well@dyntime.com>
2020-02-24 02:19:02 -0300

    Added dimension enum

---
+++ Sources/Dimension.swift
@@ -0,0 +1,53 @@
+//
+//  CodeLayout
+//
+//  Copyright (c) 2020 Wellington Marthas
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice sCollectionKit be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SCollectionKit THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import UIKit
+
+public enum Dimension {
+    case absolute(_ value: CGFloat)
+    case fractionalWidth(_ value: CGFloat)
+    case fractionalHeight(_ value: CGFloat)
+    case width
+    case height
+    
+    @inlinable
+    public func calculate(rect: CGRect = UIScreen.main.bounds) -> CGFloat {
+        switch self {
+        case let .absolute(value):
+            return value
+            
+        case let .fractionalWidth(value):
+            return rect.width * value
+            
+        case let .fractionalHeight(value):
+            return rect.height * value
+            
+        case .width:
+            return rect.width
+            
+        case .height:
+            return rect.height
+        }
+    }
+}
