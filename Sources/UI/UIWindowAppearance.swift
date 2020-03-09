7b9c57ef21bafa58d0397ed91c188efc1c92d448
Wellington Marthas <well@dyntime.com>
2020-03-09 01:24:05 -0300

    Change to use baseline

--- Sources/UI/UIWindowAppearance.swift
+++ Sources/UI/UIWindowAppearance.swift
@@ -55,7 +55,7 @@ public extension UIWindowAppearance {
             navigationBar.apply {
                 $0.barTintColor = backgroundColor
                 $0.titleTextAttributes = [ .font: font, .foregroundColor: textColor]
-
+                
                 $0.isTranslucent = false
                 $0.shadowImage = UIImage()
                 
@@ -98,25 +98,21 @@ public extension UIWindowAppearance {
     }
     
     @inlinable
-    func createNavigationBarButton(title: String, font: UIFont? = nil, positionVertical: CGFloat = 0, target: Any?, action: Selector) -> UIBarButtonItem? {
-        let attributes: [NSAttributedString.Key: Any] = [ .font: font ?? self.font, .foregroundColor: textColor, .baselineOffset: 50 ]
-
+    func createNavigationBarButton(title: String, font: UIFont? = nil, baselineOffset: CGFloat = 0, target: Any?, action: Selector) -> UIBarButtonItem? {
+        let attributes: [NSAttributedString.Key: Any] = [ .font: font ?? self.font, .foregroundColor: textColor, .baselineOffset: baselineOffset ]
+        
         return UIBarButtonItem(title: title, style: .plain, target: target, action: action).apply {
-            $0.setTitlePositionAdjustment(UIOffset(horizontal: 0, vertical: positionVertical), for: .default)
-            $0.setTitlePositionAdjustment(UIOffset(horizontal: 0, vertical: positionVertical), for: .compact)
-            $0.setTitlePositionAdjustment(UIOffset(horizontal: 0, vertical: positionVertical), for: .compactPrompt)
-            $0.setTitlePositionAdjustment(UIOffset(horizontal: 0, vertical: positionVertical), for: .defaultPrompt)
-
             $0.setTitleTextAttributes(attributes, for: .normal)
             $0.setTitleTextAttributes(attributes, for: .highlighted)
+            $0.setTitleTextAttributes(attributes, for: .selected)
+            $0.setTitleTextAttributes(attributes, for: .disabled)
         }
     }
     
     @inlinable
-    func createTabBarButton(title: String, font: UIFont, positionVertical: CGFloat) -> UITabBarItem {
+    func createTabBarButton(title: String, font: UIFont, baselineOffset: CGFloat) -> UITabBarItem {
         return UITabBarItem(title: title, image: nil, selectedImage: nil).apply {
-            $0.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: positionVertical)
-            $0.setTitleTextAttributes([ .font: font ], for: .normal)
+            $0.setTitleTextAttributes([ .font: font, .baselineOffset: baselineOffset ], for: .normal)
         }
     }
     
