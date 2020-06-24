2505de825e4cbe1af7bfe4c052eb5934fc72eae3
Wellington Marthas <well@dyntime.com>
2020-06-24 14:12:15 -0300

    Testing

--- Sources/UI/UIWindowAppearance.swift
+++ Sources/UI/UIWindowAppearance.swift
@@ -55,7 +55,7 @@ public extension UIWindowAppearance {
     func apply(on navigationBar: UINavigationBar) {
         navigationBar.barTintColor = backgroundColor
         navigationBar.titleTextAttributes = [.font: font, .foregroundColor: textColor]
-        //navigationBar.isTranslucent = false
+        navigationBar.isTranslucent = false
         navigationBar.shadowImage = UIImage()
         
         if #available(iOS 11.0, *) {
@@ -71,6 +71,10 @@ public extension UIWindowAppearance {
         collectionView.showsVerticalScrollIndicator = false
         collectionView.alwaysBounceVertical = true
         collectionView.clipsToBounds = false
+        
+        if #available(iOS 11, *) {
+            collectionView.contentInsetAdjustmentBehavior = .never
+        }
     }
     
     @inlinable
