733e01ab7246382c776869a687036dcc00d3a5a4
Wellington Marthas <well@dyntime.com>
2020-07-03 03:34:54 -0300

    Testing

--- Sources/UI/UISlidingNavigationController.swift
+++ Sources/UI/UISlidingNavigationController.swift
@@ -25,97 +25,97 @@
 import UIKit
 
 open class UISlidingNavigationController: UINavigationController {
-    private var interactiveTransition: UIPercentDrivenInteractiveTransition?
-    
-    open override func loadView() {
-        super.loadView()
-        //delegate = self
-        
-        view.addGestureRecognizer(UIScreenEdgePanGestureRecognizer(target: self, action: #selector(didEdgePan)).apply {
-            $0.maximumNumberOfTouches = 1
-            $0.edges = UIApplication.shared.leftToRightLayoutDirection ? .left : .right
-        })
-    }
-    
-    @objc
-    private func didEdgePan(_ recognizer: UIScreenEdgePanGestureRecognizer) {
-        guard let view = recognizer.view else {
-            interactiveTransition = nil
-            return
-        }
-        
-        let percent = abs(recognizer.translation(in: view).x / view.bounds.size.width)
-        
-        switch recognizer.state {
-        case .began:
-            interactiveTransition = UIPercentDrivenInteractiveTransition()
-            popViewController(animated: true)
-            
-        case .changed:
-            interactiveTransition?.update(percent)
-            
-        case .ended:
-            if percent > 0.25 || (percent > 0.15 && recognizer.velocity(in: view).y > 0) {
-                interactiveTransition?.finish()
-            }
-            else {
-                interactiveTransition?.cancel()
-            }
-            
-            interactiveTransition = nil
-            
-        default: break
-        }
-    }
+//    private var interactiveTransition: UIPercentDrivenInteractiveTransition?
+//
+//    open override func loadView() {
+//        super.loadView()
+//        delegate = self
+//
+//        view.addGestureRecognizer(UIScreenEdgePanGestureRecognizer(target: self, action: #selector(didEdgePan)).apply {
+//            $0.maximumNumberOfTouches = 1
+//            $0.edges = UIApplication.shared.leftToRightLayoutDirection ? .left : .right
+//        })
+//    }
+//
+//    @objc
+//    private func didEdgePan(_ recognizer: UIScreenEdgePanGestureRecognizer) {
+//        guard let view = recognizer.view else {
+//            interactiveTransition = nil
+//            return
+//        }
+//
+//        let percent = abs(recognizer.translation(in: view).x / view.bounds.size.width)
+//
+//        switch recognizer.state {
+//        case .began:
+//            interactiveTransition = UIPercentDrivenInteractiveTransition()
+//            popViewController(animated: true)
+//
+//        case .changed:
+//            interactiveTransition?.update(percent)
+//
+//        case .ended:
+//            if percent > 0.25 || (percent > 0.15 && recognizer.velocity(in: view).y > 0) {
+//                interactiveTransition?.finish()
+//            }
+//            else {
+//                interactiveTransition?.cancel()
+//            }
+//
+//            interactiveTransition = nil
+//
+//        default: break
+//        }
+//    }
 }
 
-extension UISlidingNavigationController: UINavigationControllerDelegate {
-    private final class SlidingTransitioning: NSObject, UIViewControllerAnimatedTransitioning {
-        private let operation: Operation
-        
-        public init(operation: Operation) {
-            self.operation = !UIApplication.shared.leftToRightLayoutDirection ? operation : (operation != .pop ? .pop : .push)
-        }
-        
-        public func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
-            return 0.25
-        }
-        
-        public func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
-            guard let fromView = transitionContext.viewController(forKey: .from)?.view,
-                  let toView = transitionContext.viewController(forKey: .to)?.view else {
-                return
-            }
-            
-            var width = transitionContext.containerView.frame.width
-            
-            if operation == .pop {
-                width = -width
-                transitionContext.containerView.insertSubview(toView, belowSubview: fromView)
-            }
-            else {
-                transitionContext.containerView.addSubview(toView)
-            }
-            
-            toView.transform = CGAffineTransform(translationX: -width , y: 0)
-            
-            UIView.animate(withDuration: transitionDuration(using: transitionContext), animations: {
-                toView.transform = .identity
-                fromView.transform = CGAffineTransform(translationX: width, y: 0)
-            }) { _ in
-                toView.transform = .identity
-                fromView.transform = .identity
-                
-                transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
-            }
-        }
-    }
-    
-    public func navigationController(_ navigationController: UINavigationController, interactionControllerFor animationController: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
-        return interactiveTransition
-    }
-    
-    public func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        return SlidingTransitioning(operation: operation)
-    }
-}
+//extension UISlidingNavigationController: UINavigationControllerDelegate {
+//    private final class SlidingTransitioning: NSObject, UIViewControllerAnimatedTransitioning {
+//        private let operation: Operation
+//        
+//        public init(operation: Operation) {
+//            self.operation = !UIApplication.shared.leftToRightLayoutDirection ? operation : (operation != .pop ? .pop : .push)
+//        }
+//        
+//        public func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+//            return 0.25
+//        }
+//        
+//        public func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+//            guard let fromView = transitionContext.viewController(forKey: .from)?.view,
+//                  let toView = transitionContext.viewController(forKey: .to)?.view else {
+//                return
+//            }
+//            
+//            var width = transitionContext.containerView.frame.width
+//            
+//            if operation == .pop {
+//                width = -width
+//                transitionContext.containerView.insertSubview(toView, belowSubview: fromView)
+//            }
+//            else {
+//                transitionContext.containerView.addSubview(toView)
+//            }
+//            
+//            toView.transform = CGAffineTransform(translationX: -width , y: 0)
+//            
+//            UIView.animate(withDuration: transitionDuration(using: transitionContext), animations: {
+//                toView.transform = .identity
+//                fromView.transform = CGAffineTransform(translationX: width, y: 0)
+//            }) { _ in
+//                toView.transform = .identity
+//                fromView.transform = .identity
+//                
+//                transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+//            }
+//        }
+//    }
+//    
+//    public func navigationController(_ navigationController: UINavigationController, interactionControllerFor animationController: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+//        return interactiveTransition
+//    }
+//    
+//    public func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+//        return SlidingTransitioning(operation: operation)
+//    }
+//}
