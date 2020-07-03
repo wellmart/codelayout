041c842aba08d7f6fa3eaf77b50504cf70763f6c
Wellington Marthas <well@dyntime.com>
2020-07-03 01:32:48 -0300

    Small changes

--- Sources/UI/UISlidingNavigationController.swift
+++ Sources/UI/UISlidingNavigationController.swift
@@ -24,48 +24,7 @@
 
 import UIKit
 
-public final class UISlidingNavigationController: UINavigationController {
-    public final class SlidingTransitioning: NSObject, UIViewControllerAnimatedTransitioning {
-        private let operation: Operation
-        
-        public init(operation: Operation) {
-            self.operation = !UIApplication.shared.leftToRightLayoutDirection ? operation : (operation != .pop ? .pop : .push)
-        }
-        
-        public func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
-            return 0.25
-        }
-        
-        public func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
-            guard let fromView = transitionContext.viewController(forKey: .from)?.view,
-                  let toView = transitionContext.viewController(forKey: .to)?.view else {
-                return
-            }
-            
-            var width = transitionContext.containerView.frame.width
-            
-            if operation == .pop {
-                width = -width
-                transitionContext.containerView.insertSubview(toView, belowSubview: fromView)
-            }
-            else {
-                transitionContext.containerView.addSubview(toView)
-            }
-            
-            toView.transform = CGAffineTransform(translationX: -width , y: 0)
-            
-            UIView.animate(withDuration: transitionDuration(using: transitionContext), animations: {
-                toView.transform = .identity
-                fromView.transform = CGAffineTransform(translationX: width, y: 0)
-            }) { _ in
-                toView.transform = .identity
-                fromView.transform = .identity
-                
-                transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
-            }
-        }
-    }
-    
+public class UISlidingNavigationController: UINavigationController {
     private var interactiveTransition: UIPercentDrivenInteractiveTransition?
     
     public override func loadView() {
@@ -112,6 +71,47 @@ public final class UISlidingNavigationController: UINavigationController {
 }
 
 extension UISlidingNavigationController: UINavigationControllerDelegate {
+    private final class SlidingTransitioning: NSObject, UIViewControllerAnimatedTransitioning {
+        private let operation: Operation
+        
+        public init(operation: Operation) {
+            self.operation = !UIApplication.shared.leftToRightLayoutDirection ? operation : (operation != .pop ? .pop : .push)
+        }
+        
+        public func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+            return 0.25
+        }
+        
+        public func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+            guard let fromView = transitionContext.viewController(forKey: .from)?.view,
+                  let toView = transitionContext.viewController(forKey: .to)?.view else {
+                return
+            }
+            
+            var width = transitionContext.containerView.frame.width
+            
+            if operation == .pop {
+                width = -width
+                transitionContext.containerView.insertSubview(toView, belowSubview: fromView)
+            }
+            else {
+                transitionContext.containerView.addSubview(toView)
+            }
+            
+            toView.transform = CGAffineTransform(translationX: -width , y: 0)
+            
+            UIView.animate(withDuration: transitionDuration(using: transitionContext), animations: {
+                toView.transform = .identity
+                fromView.transform = CGAffineTransform(translationX: width, y: 0)
+            }) { _ in
+                toView.transform = .identity
+                fromView.transform = .identity
+                
+                transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+            }
+        }
+    }
+    
     public func navigationController(_ navigationController: UINavigationController, interactionControllerFor animationController: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
         return interactiveTransition
     }
